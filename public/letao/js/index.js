3c2acd414d950a36a770a49e98ce5425021cf7d7
e <214729287@qq.com>
2018-09-13 22:10:30 +0800

    第二次提交完成搜索中心

---
+++ public/letao/js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Mac on 2018/9/12.
+ */
+$(function(){
+  var gallery = mui('.mui-slider');
+  gallery.slider({
+   interval:2000
+  });
+});
---
+++ public/letao/js/search-result.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Mac on 2018/9/13.
+ */
